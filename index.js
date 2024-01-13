de625cfcc44dff549162b27e6041d4dfa5279dfd
allankimanzi2 <allankim457@gmail.com>
2024-01-13 16:40:59 +0300

    Done assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    const thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+  }
+  
+  // Example usage:
+  const names = ["Charlie", "Samip", "Ali"];
+  const eventName = "birthday";
+  const messages = writeCards(names, eventName);
+  console.log(messages);
+
+  
+  function countDown(startingNumber) {
+    while (startingNumber >= 0) {
+      console.log(startingNumber);
+      startingNumber--;
+    }
+  }
+  
+  // Example usage:
+  countDown(10);
+  
+  
